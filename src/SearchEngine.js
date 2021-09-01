69b3efffe885938c62a97b35c8f56845e674b69e
duygugurbuzyildiz <gurbuz.duygu@gmail.com>
2021-09-01 17:04:01 +0200

    A new api to search photo was added and created a css file to arrange the appearance of the photos.

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,15 +1,21 @@
 import React, {useState} from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./SearchEngine.css";
 import 'bootstrap/dist/css/bootstrap.css';
 
 export default function SearchEngine(){
     let [keyword, setKeyword] = useState(null);
     let [result, setResult] = useState(null);
+    let [photos, setPhotos] = useState (null);
 
     function handleResponse(response){
         setResult(response.data[0]);
+    }
+
+    function handleImage(response){
+        setPhotos(response.data.photos);
     }
 
     function submit(event){
@@ -17,6 +23,11 @@ export default function SearchEngine(){
 
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
         axios.get(apiUrl).then(handleResponse);
+
+        let pexelsApiKey = "563492ad6f917000010000012781662f9bed46bd98879a0ba89b2619";
+        let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
+        
+        axios.get(pexelsApiUrl, {headers: {Authorization: `Bearer ${pexelsApiKey}`}}).then(handleImage);
     }
 
     function handleKeywordChange(event){
@@ -31,6 +42,7 @@ export default function SearchEngine(){
             </form>
         </section>
         <Results result={result}/>
+        <Photos photos={photos}/>
         
     </div>);
 }
